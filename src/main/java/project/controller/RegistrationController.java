94358566df94b743bab8b10399f6d498d0ee012d
sakkattack <isakkolbeins@gmail.com>
2018-11-27 16:50:11 +0000

    registration form,,,, not saving users yet

--- src/main/java/project/controller/RegistrationController.java
+++ src/main/java/project/controller/RegistrationController.java
@@ -3,9 +3,14 @@ package project.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import project.persistence.entities.User;
 import project.service.UserManagementService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Small controller just to show that you can have multiple controllers
@@ -16,23 +21,42 @@ public class RegistrationController {
 
 
     //Instance Variables
-    private project.service.UserManagementService UserManagementService;
+    private UserManagementService userManagementService;
 
     //Dependency Injection
     @Autowired
-    public RegistrationController(UserManagementService UserManagementService) {
-        this.UserManagementService = UserManagementService;
+    public RegistrationController(UserManagementService userManagementService) {
+        this.userManagementService = userManagementService;
     }
 
     @RequestMapping(value = "/registration", method = RequestMethod.GET)
     public String newRegistration(Model model){
+
+        model.addAttribute("newUser", new User());
+
         return "registration";
     }
 
     @RequestMapping(value = "/registration", method = RequestMethod.POST)
-    public String saveRegistration(Model model){
-        return "registration";
+    public String saveRegistration(@ModelAttribute("newUser") User newUser , Model model){
+
+        List<String> errors = new ArrayList();
+
+        User usernameTaken = userManagementService.findByUserName(newUser.getUserName());
+        if (usernameTaken != null) {
+            errors.add("The username " + newUser.getUserName() + " is already taken");
+        }
+
+        if (!errors.isEmpty()) {
+            model.addAttribute("errors", errors);
+            model.addAttribute("newUser", newUser);
+            return "registration";
+        }
+        else {
+            userManagementService.save(newUser);
+            model.addAttribute("success", "User has been registered successfully");
+            model.addAttribute("newUser", newUser);
+            return "registration";
+        }
     }
-
-
 }
--- src/main/webapp/WEB-INF/jsp/registration.jsp
+++ src/main/webapp/WEB-INF/jsp/registration.jsp
@@ -3,6 +3,7 @@
 <%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<%@ taglib prefix="sf" uri="http://www.springframework.org/tags/form" %>
 <%@ taglib prefix="form" uri="http://www.springframework.org/tags/form" %>
 
 <c:set var="contextPath" value=""/>
@@ -15,7 +16,42 @@
     <body>
 
 
-    <h1>Registration form </h1>
+    <h2>Registration form </h2>
+
+    <sf:form method="POST" modelAttribute="newUser" action="new">
+
+        <table>
+            <tr>
+                <td>Username:</td>
+                    <%--the `path` attribute matches the `name` attribute of the Entity that was passed in the model--%>
+                <td><sf:input path="userName" type="text" placeholder="Username"/></td>
+            </tr>
+            <tr>
+                <td>First name:</td>
+                    <%--the `path` attribute matches the `note` attribute of the Entity that was passed in the model--%>
+                <td><sf:input path="firstName" type="text" placeholder="First name"/></td>
+            </tr>
+            <tr>
+                <td>Last name:</td>
+                    <%--the `path` attribute matches the `note` attribute of the Entity that was passed in the model--%>
+                <td><sf:input path="lastName" type="text" placeholder="Last name"/></td>
+            </tr>
+            <tr>
+                <td>Email:</td>
+                    <%--the `path` attribute matches the `note` attribute of the Entity that was passed in the model--%>
+                <td><sf:input path="email" type="email" placeholder="Email"/></td>
+            </tr>
+            <tr>
+                <td>Password:</td>
+                    <%--the `path` attribute matches the `note` attribute of the Entity that was passed in the model--%>
+                <td><sf:input path="password" type="password" placeholder="Password"/></td>
+            </tr>
+
+        </table>
+
+        <input type="submit" VALUE="Save"/>
+
+    </sf:form>
 
     </body>
     <footer></footer>
